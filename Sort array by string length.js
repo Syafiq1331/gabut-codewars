42231b64064a2e3f19d876516de3e2dc83cf14e7
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-09-14 09:26:58 +0700

    solved : Sort array by string length

--- Sort array by string length.js
+++ Sort array by string length.js
@@ -10,3 +10,9 @@ Your function would return the following array:
 
 All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
 */
+
+// Solution
+
+const sortByLength = (a) => a.sort((a, b) => a.length - b.length);;
+
+console.log(sortByLength(["", "Moderately", "Brains", "Pizza"]))
